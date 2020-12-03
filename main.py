c60c5a440af1198998e47f3c06a387ae3a8f7bbc
MIKE HAN <bravenw148@gmail.com>
2020-12-03 22:12:14 +0000

    change to random choice

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import random
 names_string = input("Give me everybody's names, seperated by a comma. ")
 names = names_string.split(",")
 
-num_names = len(names)
+# num_names = len(names)
 
-random_name = random.randint(0,num_names-1)
+# random_name = random.randint(0,num_names-1)
 
-who_pay_the_bill = names[random_name]
+# who_pay_the_bill = names[random_name]
+
+who_pay_the_bill = random.choice(names)
 print (who_pay_the_bill+" is going to buy the bill today")
